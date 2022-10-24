0dc99d4f3c3eff7ec016184cd7fdb0bc9aaa6f0c
quilo-bikcodes <quilo.bikcodes@gmail.com>
2022-10-24 22:07:55 +0530

    Initialize Repo

---
+++ main.py
@@ -0,0 +1,24 @@
+from pydrive.auth import GoogleAuth
+from pydrive.drive import GoogleDrive
+import pandas as pd
+  
+# Initializing a GoogleAuth Object
+gauth = GoogleAuth()
+  
+# client_secrets.json file is verified
+# and it automatically handles authentication
+gauth.LocalWebserverAuth()
+  
+# GoogleDrive Instance is created using
+# authenticated GoogleAuth instance
+drive = GoogleDrive(gauth)
+  
+# Initialize GoogleDriveFile instance with file id
+file_obj = drive.CreateFile({'id': '12vtkiUGj-DKpGX4_2GG4BC7cA3auqwIL_56GGA8gO5s'})
+file_obj.GetContentFile('Data Collect (Responses).xls',
+         mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+  
+dataframe = pd.read_excel('Data Collect (Responses).xls')
+print(dataframe)
+
+# 12vtkiUGj-DKpGX4_2GG4BC7cA3auqwIL_56GGA8gO5s
